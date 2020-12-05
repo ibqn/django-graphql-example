97c10e0c4bff0498c8172550bec2b47472dbaef7
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2020-12-05 01:05:36 +0100

    add more types and resolvers

--- backend_api/schema.py
+++ backend_api/schema.py
@@ -1,7 +1,20 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from backend_api.models import Company, Car
+from backend_api.models import Company, Car, Sharing
+from django.contrib.auth.models import User
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "sharing")
+
+
+class SharingType(DjangoObjectType):
+    class Meta:
+        model = Sharing
+        fields = ("id", "user", "car", "created", "updated", "until")
 
 
 class CompanyType(DjangoObjectType):
@@ -13,12 +26,24 @@ class CompanyType(DjangoObjectType):
 class CarType(DjangoObjectType):
     class Meta:
         model = Car
-        fields = ("id", "name", "notes", "company")
+        fields = ("id", "name", "notes", "company", "sharing")
 
 
 class Query(graphene.ObjectType):
     all_cars = graphene.List(CarType)
+    all_users = graphene.List(UserType)
+    all_sharing = graphene.List(SharingType)
+    all_companies = graphene.List(CompanyType)
     company_by_name = graphene.Field(CompanyType, name=graphene.String(required=True))
+
+    def resolve_all_sharing(root, info):
+        return Sharing.objects.all()
+
+    def resolve_all_users(root, info):
+        return User.objects.all()
+
+    def resolve_all_companies(root, info):
+        return Company.objects.all()
 
     def resolve_all_cars(root, info):
         return Car.objects.select_related("company").all()
