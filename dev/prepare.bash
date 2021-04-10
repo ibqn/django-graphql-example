2253b43e5a9b51abf7983b96b6ad72b018a4d9bc
ibqn <evgen.ibqn@gmail.com>
2021-04-11 00:35:10 +0200

    update prepare script to use poetry. include lock file. update deps.

--- dev/prepare.bash
+++ dev/prepare.bash
@@ -3,13 +3,16 @@
 set -Eeuxo pipefail
 
 DIR="$(dirname "$(readlink -f "$0")")"
+pushd ${DIR}
 
-python3 -m venv "${DIR}/venv"
+python3 -m venv "venv"
 
-source "${DIR}/venv/bin/activate"
+source "venv/bin/activate"
 
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade setuptools
 python3 -m pip install --upgrade wheel
 
-python3 -m pip install --upgrade --requirement "${DIR}/requirements.txt"
+#python3 -m pip install --upgrade --requirement "${DIR}/requirements.txt"
+poetry install
+popd
